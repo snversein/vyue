f076a42c64acdd240f1eb9bcde0975aff796d06c
nikhildubey-23 <ravannick0089@gmail.com>
2026-01-28 13:44:22 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
